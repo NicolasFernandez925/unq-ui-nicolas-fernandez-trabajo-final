5274c878035d479c08c51e90f8dee5b3102a44d1
nicolas nahuel <nicolasnahuel.pr@gmail.com>
2022-06-24 20:19:16 -0300

    style: add style page home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import ImageGame from "assets/game.png"
+import { Container, Col, Row } from 'react-bootstrap'
+
+import './style.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='container-home'>
+      <Container fluid="lg">
+        <Row className='d-flex justify-content-center'>
+          <Col xs={8} md={4}> 
+            <img className='image-logo-game rotate' src={ImageGame} alt='img-game' />
+          </Col>
+        </Row>
+        <Row className='mt-5'>
+          <h2 className='text-center text_select-mode'>Select mode</h2>
+          <Col className='d-flex justify-content-center mt-5'> 
+              <button className='button_select-player' type='button'>1 player</button>
+              <button className='button_select-player' type='button'>2 player</button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
