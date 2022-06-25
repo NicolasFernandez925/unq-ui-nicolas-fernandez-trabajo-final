27de061b1ac5fab31964b2187a73722629232fec
nicolas nahuel <nicolasnahuel.pr@gmail.com>
2022-06-24 22:28:49 -0300

    style: add style modal

--- src/components/common/Modal/index.jsx
+++ src/components/common/Modal/index.jsx
@@ -1,29 +1,53 @@
 import React from 'react'
+import Tijera from 'assets/tijera.png'
+import Lagarto from 'assets/lagarto.png'
+import Papel from 'assets/papel.png'
+import Spock from 'assets/spock.png'
+import Piedra from 'assets/piedra.png'
 import { Button, Modal } from 'react-bootstrap'
+
+import "./style.css"
 
 const CustomModal = (props) => {
   return (
     <Modal
       {...props}
-      size="lg"
+      size="md"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          Rules
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+          <div>
+            <div className='d-flex align-items-center'>
+              <img className='image-game-rules' src={Tijera} alt='img-game' />       
+              <p className='m-0'>Tijera</p>
+            </div>
+            
+            <div className='d-flex align-items-center'>
+              <img className='image-game-rules' src={Lagarto} alt='img-game' />
+              <p className='m-0'>Lagarto</p>
+            </div>
+            <div className='d-flex align-items-center'>
+              <img className='image-game-rules' src={Spock} alt='img-game' />
+              <p className='m-0'>Spock</p>
+            </div>
+            <div className='d-flex align-items-center'>
+              <img className='image-game-rules' src={Papel} alt='img-game' />
+              <p className='m-0'>Papel</p>
+            </div>
+            <div className='d-flex align-items-center'>
+              <img className='image-game-rules' src={Piedra} alt='img-game' />
+              <p className='m-0'>Piedra</p>
+            </div>  
+          </div>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+        <Button variant="dark" onClick={props.onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
   )
