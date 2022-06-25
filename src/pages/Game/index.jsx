55c11621626866f0a41bfb2451fd4538a073fec7
Nicolas Fernandez <nicolasfernandez@MacBook-Air-de-Nicolas.local>
2022-06-24 23:45:01 -0300

    style:the styles for the home for 1 player are finished

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -7,16 +7,39 @@ import Piedra from 'assets/piedra.png'
 import { Col, Container, Row } from 'react-bootstrap'
 
 import "./style.css"
+import { useNavigate } from 'react-router-dom'
 
 const Game = () => {
+
+  const navigate = useNavigate()
+
+  const handleGoBack = () => {
+    navigate(-1)
+  }
+
   return (
     <Container fluid="lg">
-        <Row className="d-flex justify-content-center">
+        <span role='button' onClick={handleGoBack} className='btn_go-back'>Go back</span>
+        <Row className="d-flex justify-content-center align-items-center">
           <Col xs={5} lg={2}>
             <div className='selected-image'></div>
           </Col>
+          <Col xs={2} lg={1} className='text-center'>
+              <span className='text_vs'>VS</span>
+          </Col>        
           <Col xs={5} lg={2}>
             <div className='selected-image'></div>    
+          </Col>
+        </Row>
+        <Row className="d-flex justify-content-center align-items-center mt-5">
+          <Col xs={2} className='text-center'>
+            <div><p className='m-0 text-white'>Won</p></div>
+          </Col>
+          <Col xs={2}className='text-center'>
+              <div><p className='m-0 text-white'>Tied</p></div>
+          </Col>        
+          <Col xs={2}className='text-center'>
+            <div><p className='m-0 text-white'>Lost</p></div>   
           </Col>
         </Row>
         <Row className="d-flex justify-content-center container_images-selected">
