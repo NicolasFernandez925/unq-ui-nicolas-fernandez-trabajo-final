1a12549e842206a08e58ca9d478478d156d309ea
Nicolas Fernandez <nicolasfernandez@MacBook-Air-de-Nicolas.local>
2022-06-24 23:01:39 -0300

    feat: modal rules finish

--- src/components/common/Modal/index.jsx
+++ src/components/common/Modal/index.jsx
@@ -23,27 +23,56 @@ const CustomModal = (props) => {
       </Modal.Header>
       <Modal.Body>
           <div>
-            <div className='d-flex align-items-center'>
-              <img className='image-game-rules' src={Tijera} alt='img-game' />       
-              <p className='m-0'>Tijera</p>
+            <div className='d-flex justify-content-between align-items-center'>
+              <div className='d-flex align-items-center'>
+                <img className='image-game-rules' src={Tijera} alt='img-game' />       
+                <p className='m-0'>Tijera</p>
+              </div>
+              <div>
+                 <p className='m-0'><span className='text-gana'>Gana: </span>Papel, lagarto</p>
+                 <p className='m-0'><span className='text-pierde'>Pierde: </span>Piedra, spock</p> 
+              </div>
             </div>
-            
-            <div className='d-flex align-items-center'>
-              <img className='image-game-rules' src={Lagarto} alt='img-game' />
-              <p className='m-0'>Lagarto</p>
+            <div className='d-flex justify-content-between align-items-center'>
+              <div className='d-flex align-items-center'>
+                <img className='image-game-rules' src={Lagarto} alt='img-game' />
+                <p className='m-0'>Lagarto</p>
+              </div>
+              <div>
+                 <p className='m-0'><span className='text-gana'>Gana: </span>Papel, Spock</p>
+                 <p className='m-0'><span className='text-pierde'>Pierde: </span>Tijera, Piedra</p> 
+              </div>
             </div>
-            <div className='d-flex align-items-center'>
-              <img className='image-game-rules' src={Spock} alt='img-game' />
-              <p className='m-0'>Spock</p>
+            <div className='d-flex justify-content-between align-items-center '>
+              <div className='d-flex align-items-center'>
+                <img className='image-game-rules' src={Spock} alt='img-game' />
+                <p className='m-0'>Spock</p>
+              </div>
+              <div>
+                 <p className='m-0'><span className='text-gana'>Gana: </span>Tijera, Piedra</p>
+                 <p className='m-0'><span className='text-pierde'>Pierde: </span>Papel, Lagarto</p> 
+              </div>
             </div>
-            <div className='d-flex align-items-center'>
-              <img className='image-game-rules' src={Papel} alt='img-game' />
-              <p className='m-0'>Papel</p>
+            <div className='d-flex justify-content-between align-items-center'>
+              <div className='d-flex align-items-center'>
+                <img className='image-game-rules' src={Papel} alt='img-game' />
+                <p className='m-0'>Papel</p>
+              </div>
+              <div>
+                 <p className='m-0'><span className='text-gana'>Gana: </span>Piedra, Spock</p>
+                 <p className='m-0'><span className='text-pierde'>Pierde: </span>Tijera, Lagarto</p> 
+              </div>
             </div>
-            <div className='d-flex align-items-center'>
-              <img className='image-game-rules' src={Piedra} alt='img-game' />
-              <p className='m-0'>Piedra</p>
-            </div>  
+            <div className='d-flex justify-content-between align-items-center'>
+              <div className='d-flex align-items-center'>
+                <img className='image-game-rules' src={Piedra} alt='img-game' />
+                <p className='m-0'>Piedra</p>
+              </div>
+              <div>
+                 <p className='m-0'><span className='text-gana'>Gana: </span>Tijera, lagarto</p>
+                 <p className='m-0'><span className='text-pierde'>Pierde: </span>Papel, spock</p> 
+              </div>
+            </div>
           </div>
       </Modal.Body>
       <Modal.Footer>
